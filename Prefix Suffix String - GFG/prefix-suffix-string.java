26a284347bc93756f0ded4d5988e6d1c3f1a57e4
Saurabh-269 <115521321+Saurabh-269@users.noreply.github.com>
2023-04-21 00:46:59 +0530

    Added solution - LeetHub

---
+++ Prefix Suffix String - GFG/prefix-suffix-string.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+
+public class code1 
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            String s1[]=in.readLine().trim().split(" ");
+            String s2[]=in.readLine().trim().split(" ");
+
+            Solution ob=new Solution();
+            int ans=ob.prefixSuffixString(s1,s2);
+            out.println(ans);
+
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+  public int prefixSuffixString(String s1[],String s2[])
+    {
+        int ans=0;
+        for(int i=0;i<s2.length;i++){
+              
+              for(int j=0;j<s1.length;j++){
+                  if(s1[j].length()!=s2[i].length () && s1[j].contains(s2[i])){
+                        
+                        if(s1[j].indexOf(s2[i])==0){
+                           
+                            ans++;
+                            break;
+                            
+                        }
+                        if(s1[j].substring(s1[j].length()-s2[i].length()).equals(s2[i])){
+                          ans++;
+                           break;
+                           
+                        }
+                       
+                         
+                  }
+                 
+              }
+        }
+        return ans;
+        
+        
+        
+        
+    }
+}
